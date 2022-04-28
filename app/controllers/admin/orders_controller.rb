d557576b6a995bbd344b3155eeac94cc2b61dc91
EC2 Default User <ec2-user@ip-172-31-83-32.ec2.internal>
2022-04-28 12:59:34 +0000

    Update Order9

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,7 +13,7 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    redirect_to '/admin/orders'
+    redirect_to admin_order_path(@order)
   end
 
   private
