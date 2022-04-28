4c2c12995cd626cd66d40b2bd0b1c71a1b136189
EC2 Default User <ec2-user@ip-172-31-83-32.ec2.internal>
2022-04-28 04:11:52 +0000

    Update Order4

--- db/migrate/20220420093753_create_orders.rb
+++ db/migrate/20220420093753_create_orders.rb
@@ -4,7 +4,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
      t.integer :customer_id, null: false
      t.integer :shipping_cost, default: 800, null: false
      t.integer :total_payment, default: 0, null: false
-     t.integer :payment_mehod, default: 0, null: false
+     t.integer :payment_method, default: 0, null: false
      t.integer :status, null: false
      t.string :name, null: false
      t.string :address, null: false
