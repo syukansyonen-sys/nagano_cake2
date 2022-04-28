62bdd0edc7e4d128a6866b3cd2759e0672317cbc
EC2 Default User <ec2-user@ip-172-31-83-32.ec2.internal>
2022-04-28 10:50:45 +0000

    Update Order8

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,5 +3,13 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
 
   enum making_status: { "製作不可":0, "製作待ち":1, "製作中":2, "製作完了":3 }
-end
 
+
+  class << self
+    def localed_making_statuses
+      making_statuses.keys.map do |s|
+        [ApplicationController.helpers.t("making_status.article.#{s}"), s]
+      end
+    end
+  end
+end
